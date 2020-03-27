973d47fbb7ef905c4a573f9c945fb9d2b516f297
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-27 13:11:50 -0300

    Vídeo 2.3

---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const Usuario = require('./usuarios-modelo');
+
+const { InvalidArgumentError } = require('../erros');
+
+const bcrypt = require('bcrypt');
+
+function verificaUsuario(usuario) {
+  if (!usuario) {
+    throw new InvalidArgumentError('Não existe usuário com esse e-mail!');
+  }
+}
+
+async function verificaSenha(senha, senhaHash) {
+  const senhaValida = await bcrypt.compare(senha, senhaHash);
+  if (!senhaValida) {
+    throw new InvalidArgumentError('E-mail ou senha inválidos!');
+  }
+}
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'senha',
+      session: false
+    },
+    async (email, senha, done) => {
+      try {
+        const usuario = await Usuario.buscaPorEmail(email);
+        verificaUsuario(usuario);
+        verificaSenha(senha, usuario.senhaHash);
+
+        done(null, usuario);
+      } catch (erro) {
+        done(erro);
+      }
+    }
+  )
+);

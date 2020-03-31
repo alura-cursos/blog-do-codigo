b834f949d78893299cecab9fb26abb345f4fce91
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-31 15:51:24 -0300

    Vídeo 3.3

--- src/posts/posts-rotas.js
+++ src/posts/posts-rotas.js
@@ -1,8 +1,12 @@
 const postsControlador = require('./posts-controlador');
+const passport = require('passport');
 
 module.exports = app => {
   app
     .route('/post')
     .get(postsControlador.lista)
-    .post(postsControlador.adiciona);
+    .post(
+      passport.authenticate('bearer', { session: false }),
+      postsControlador.adiciona
+    );
 };
--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -14,5 +14,10 @@ module.exports = app => {
     .post(usuariosControlador.adiciona)
     .get(usuariosControlador.lista);
 
-  app.route('/usuario/:id').delete(usuariosControlador.deleta);
+  app
+    .route('/usuario/:id')
+    .delete(
+      passport.authenticate('bearer', { session: false }),
+      usuariosControlador.deleta
+    );
 };

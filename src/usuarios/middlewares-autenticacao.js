5a37cb3fed0f29cff9662e949e7ec953b73ab0fa
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-31 16:46:39 -0300

    Vídeo 3.5

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -22,5 +22,28 @@ module.exports = {
         return next();
       }
     )(req, res, next);
+  },
+
+  bearer: (req, res, next) => {
+    passport.authenticate(
+      'bearer',
+      { session: false },
+      (erro, usuario, info) => {
+        if (erro && erro.name === 'JsonWebTokenError') {
+          return res.status(401).json({ erro: erro.message });
+        }
+
+        if (erro) {
+          return res.status(500).json({ erro: erro.message });
+        }
+
+        if (!usuario) {
+          return res.status(401).json();
+        }
+
+        req.user = usuario;
+        return next();
+      }
+    )(req, res, next);
   }
 };

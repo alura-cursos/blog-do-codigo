19e1f9f7582b715dbcd6e7e51a56097d5fc3b6df
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-31 15:51:24 -0300

    Vídeo 3.3

--- src/usuarios/estrategias-autenticacao.js
+++ src/usuarios/estrategias-autenticacao.js
@@ -1,11 +1,13 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
 
 const Usuario = require('./usuarios-modelo');
 
 const { InvalidArgumentError } = require('../erros');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 function verificaUsuario(usuario) {
   if (!usuario) {
@@ -40,3 +42,17 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new BearerStrategy(
+    async (token, done) => {
+      try {
+        const payload = jwt.verify(token, process.env.CHAVE_JWT);
+        const usuario = await Usuario.buscaPorId(payload.id);
+        done(null, usuario);
+      } catch (erro) {
+        done(erro);
+      }      
+    }
+  )
+)

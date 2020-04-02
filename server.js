284dc6983ee4645c857a7e7a504e455aaf1c909c
Andrew Ijano <andrew.lopes@uol.com.br>
2020-04-02 16:52:14 -0300

    Vídeo 4.2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const app = require('./app');
 const port = 3000;
 const db = require('./database');
+require('./redis/blacklist');
 
 const routes = require('./rotas');
 routes(app);
